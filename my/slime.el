ce21fadd5c0bc2ca9573416bc2f8e709a5a68a14
Moritz Heidkamp <moritz@twoticketsplease.de>
2012-01-18 11:22:43 +0100

    set slime-net-coding-system to 'utf-8-unix (thanks max)

--- my/slime.el
+++ my/slime.el
@@ -1,9 +1,14 @@
 (eval-after-load 'slime
-  '(let ((keys
-          '(("M-n"     . forward-paragraph)
-            ("M-p"     . backward-paragraph))))
+  '(progn
+     (let ((keys
+            '(("M-n"     . forward-paragraph)
+              ("M-p"     . backward-paragraph))))
 
-     (dolist (key keys)
-       (define-key slime-mode-map
-         (read-kbd-macro (car key))
-         (cdr key)))))
+       (dolist (key keys)
+         (define-key slime-mode-map
+           (read-kbd-macro (car key))
+           (cdr key))))
+
+     ;; Avoids that some unicode characters blow up the Slime connection
+     ;; to the swank server
+     (setq slime-net-coding-system 'utf-8-unix)))
