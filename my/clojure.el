3df695ae1fa67c57d37fbf8edf301e29c7a82079
Moritz Heidkamp <moritz@twoticketsplease.de>
2012-01-18 11:19:52 +0100

    some clojure config cleanup

--- my/clojure.el
+++ my/clojure.el
@@ -2,19 +2,11 @@
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
 
-(add-hook 'clojure-mode-hook (lambda () 
-                               (show-paren-mode t)
-                               (paredit-mode t)
-                               (highlight-parentheses-mode t)))
-
-
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/swank-clojure")
-(require 'swank-clojure)
-
-(setq swank-clojure-jar-path "/usr/share/clojure/clojure.jar"
-      swank-clojure-extra-classpaths 
-      '("/usr/share/emacs/site-lisp/swank-clojure/src/swank"
-        "/usr/share/clojure/clojure-contrib.jar"))
+(add-hook 'clojure-mode-hook
+          (lambda () 
+            (show-paren-mode t)
+            (paredit-mode t)
+            (highlight-parentheses-mode t)))
 
 (eval-after-load 'clojure-mode
   '(define-clojure-indent
@@ -28,6 +20,5 @@
      (let-uri-parts 'defun)))
 
 ;; slime
-(eval-after-load "slime" 
+(eval-after-load 'slime
   '(progn (slime-setup '(slime-repl))))
- 
