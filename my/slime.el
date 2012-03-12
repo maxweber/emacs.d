c5d9427a7cb18afe2370f078405872303314592e
Max Weber <max@doo.net>
2012-03-12 09:45:11 +0100

    Two key bindings has been added, which allows you to connect quickly to a swank server.

--- my/slime.el
+++ my/slime.el
@@ -12,3 +12,17 @@
      ;; Avoids that some unicode characters blow up the Slime connection
      ;; to the swank server
      (setq slime-net-coding-system 'utf-8-unix)))
+
+(defun slime-connecting (port)
+  (lexical-let ((port port))
+      (lambda ()
+        (interactive)
+        (slime-connect "localhost" port))))
+
+(define-key (current-global-map)
+  (kbd "C-c 1")
+  (slime-connecting 4005))
+
+(define-key (current-global-map)
+  (kbd "C-c 2")
+  (slime-connecting 4006))
