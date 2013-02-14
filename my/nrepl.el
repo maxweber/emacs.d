e1bd0c3ab3220ef8baf4bf4d33fa737decc5dc86
Max Weber <max@doo.net>
2013-02-14 15:19:04 +0100

    nrepl-switch-to-next-connection has been added, which switch the active connection to the next connection entry in nrepl-connection-list.

--- my/nrepl.el
+++ my/nrepl.el
@@ -7,3 +7,16 @@
 (set-variable 'nrepl-use-pretty-printing t)
 (global-set-key (kbd "C-c M-p") 'nrepl-set-ns)
 
+(defun nrepl-current-connection-infos ()
+  (with-current-buffer (get-buffer (nrepl-current-connection-buffer))
+    nrepl-endpoint))
+
+(defun nrepl-rotate-connections ()
+  (interactive)
+  (setq nrepl-connection-list
+        (append (rest nrepl-connection-list)
+                (list (first nrepl-connection-list))))
+  (message (apply 'format "Active nrepl connection: Host=%s Port=%s"
+                  (nrepl-current-connection-infos))))
+
+(global-set-key (kbd "C-c C-x n") 'nrepl-rotate-connections)
