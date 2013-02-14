54327034d9ef362d9ffd98ee686707ebc885f2fc
Max Weber <max@doo.net>
2013-02-14 10:10:53 +0100

    nrepl config has been adapted

--- my/nrepl.el
+++ my/nrepl.el
@@ -2,7 +2,6 @@
 
 (add-hook 'nrepl-mode-hook 'paredit-mode)
 (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
-(add-to-list 'same-window-buffer-names "*nrepl*")
 (add-hook 'nrepl-mode-hook 'paredit-mode)
 
 (set-variable 'nrepl-use-pretty-printing t)
