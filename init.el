c3a212f7608fb012c992f81bbcb00894e2e36423
Maximilian Weber <weber.maximilian@googlemail.com>
2016-08-22 11:32:06 +0200

    Using melpa instead of marmalade

--- init.el
+++ init.el
@@ -1,12 +1,10 @@
 (require 'package)
 
-(add-to-list
- 'package-archives
- '("marmalade" . "http://marmalade-repo.org/packages/"))
-
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (require 'cl)
