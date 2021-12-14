d111cc3644942b59dfe6c51afc168a5306c62b8e
Max Weber <weber.maximilian@gmail.com>
2021-12-14 16:54:40 +0100

    Using different theme when running emacs from a terminal.

--- my/theme.el
+++ my/theme.el
@@ -3,4 +3,13 @@
 
 ;(color-theme-blackboard)
 
-(load-file "~/.emacs.d/themes/spacegray-theme.el")
+;; necessary since Emacs 27, see: https://emacs.stackexchange.com/a/52804
+(setq custom--inhibit-theme-enable nil)
+
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+
+(if (display-graphic-p)
+    (load-file "~/.emacs.d/themes/spacegray-theme.el")
+  ;; use different theme for emacs -nw
+  (load-theme 'nord t)
+  )
